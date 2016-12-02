9ee118ed878d7363e68016783bb10ebc7ebad730
cabrel <robin.harper@sedarasecurity.com>
2016-12-02 13:31:47 -0600

    added user create statements to sql

--- alienvault-asset-tracker.sql
+++ alienvault-asset-tracker.sql
@@ -1,3 +1,7 @@
+-- The password you use on the following two lines needs to be added to the alienvault-asset-tracker.cfg file as well in the password= field
+GRANT SELECT ON alienvault.host, to hostchecker@'127.0.0.1' IDENTIFIED BY '<replace with password>';
+GRANT SELECT ON alienvault.host_ip, to hostchecker@'127.0.0.1' IDENTIFIED BY '<replace with password>';
+
 DELETE FROM plugin WHERE id = "717354";
 DELETE FROM plugin_sid where plugin_id = "717354";
 INSERT INTO plugin (id, type, name, description, product_type, vendor) VALUES (717354, 1, 'alienvault-asset-tracker', 'AlienVault Asset Tracker', 4, 'AlienVault Asset Tracker');
